bd027b99a1d6ce4d57b53118e113d479bb55e6cb
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2021-12-03 21:24:59 +0900

    svelte

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Hello from "./components/Hello.svelte";
+
+  function onHello(e) {
+    console.log(e);
+  }
+</script>
+
+<Hello on:hello={onHello} />
---
+++ svelte-app/src/components/Hello.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  function onClick(e) {
+    console.log(e);
+    dispatch("hello", { message: "Hello Svelte!" });
+  }
+</script>
+
+<p>Hello Svelte!</p>
+<button on:click={onClick}>HELLO</button>
