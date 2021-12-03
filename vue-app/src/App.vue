94bd3867e3384abff4492a296ed96e6f0703d67a
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2021-12-03 20:36:57 +0900

    vue

---
+++ vue-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Hello @hello="onHello" />
+</template>
+
+<script>
+import Hello from './components/Hello.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Hello
+  },
+  methods: {
+    onHello(event) {
+      console.log(event);
+    }
+  }
+}
+</script>
---
+++ vue-app/src/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>Hello Vue!</p>
+  <button @click="onClick">HELLO</button>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  emits: ['hello'],
+  methods: {
+    onClick(event) {
+      console.log(event);
+      this.$emit('hello', { message: 'hello Vue!' })
+    },
+  },
+}
+</script>
